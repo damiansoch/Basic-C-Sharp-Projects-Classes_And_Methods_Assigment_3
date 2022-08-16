a04c7ffea434e55a14152f8c1c4e74da7a265309
damiansoch <66294165+damiansoch@users.noreply.github.com>
2022-08-16 09:51:34 +0100

    Add project files.

---
+++ Classes_And_Methods_Assigment_3/Class1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Classes_And_Methods_Assigment_3
+{
+    public class Class1
+    {
+        public void AssigmMeth(int numberOne, int NumberTwo)
+        {
+            double result = Math.Sqrt(numberOne);
+
+            Console.WriteLine("second parameter is: {0}", NumberTwo);
+        }
+    }
+}
---
+++ Classes_And_Methods_Assigment_3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Classes_And_Methods_Assigment_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 class1 = new Class1();
+
+            class1.AssigmMeth(numberOne: 3, NumberTwo: 5);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
